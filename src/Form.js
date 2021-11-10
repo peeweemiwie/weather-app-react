7c68e76f6368647728d2c78f79b0e6e5dc95b66b
peeweemiwie <miwayagi@gmail.com>
2021-11-10 12:52:38 -0500

    Remove ids. Add type submit to buttons. Create onchange and onsubmit functions

--- src/Form.js
+++ src/Form.js
@@ -1,44 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from './components/Button';
 import './Form.scss';
 
 const Form = () => {
+	const [city, setCity] = useState('');
+	const changeHandler = (event) => {
+		setCity(event.target.value);
+	};
+	const submitHandler = (event) => {
+		event.preventDefault();
+	};
 	return (
-		<form className='Form'>
+		<form className='Form' onSubmit={submitHandler}>
 			<div className='container-input-search'>
 				<input
 					className='input-text'
 					data-units='imperial'
-					id='input-search'
 					type='text'
-					name=''
 					placeholder='Enter city name'
-					value=''
+					onChange={changeHandler}
 				/>
 				<Button
 					className='mk-btn-primary'
+					type='submit'
 					units='imperial'
-					id='submit-search'
 					value='Submit'
 				/>
 			</div>
 			<div className='container-current-location-unit'>
 				<Button
 					className='mk-btn-outline-primary'
+					type='submit'
 					units='imperial'
-					id='submit-current-location'
 					value='Current Location'
 				/>
 				<Button
 					className='mk-btn-outline-secondary'
+					type='submit'
 					units='imperial'
-					id='submit-imperial'
 					value='F'
 				/>
 				<Button
 					className='mk-btn-outline-secondary'
+					type='submit'
 					units='metric'
-					id='submit-metric'
 					value='C'
 				/>
 			</div>
