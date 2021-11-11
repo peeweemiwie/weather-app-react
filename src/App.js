e1a2346dd02d2fff2ad59ee9af8913056716150b
peeweemiwie <miwayagi@gmail.com>
2021-11-11 16:38:44 -0500

    Create dummy data to display onload.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,22 @@ import Form from './Form';
 import Current from './current/Current';
 import Forecast from './forecast/Forecast';
 import Footer from './Footer';
+
+const DUMMY_DATA = {
+	city: 'new york',
+	temperature: 58,
+	feelsLike: 57,
+	weather: 'Clouds',
+	description: 'broken clouds',
+	humidity: 70,
+	wind: 1.01,
+	icon: '04d',
+	dt: 1636666149,
+	loaded: true,
+};
+
 function App() {
+	// WIP - use DUMMY_DATA instead
 	const [updatedWeatherData, setUpdatedWeatherData] = useState('');
 	const storeDataHandler = (weatherData) => {
 		setUpdatedWeatherData({ ...weatherData });
@@ -14,7 +29,7 @@ function App() {
 		<div className='App' data-units='imperial'>
 			<Form onReceiveRequest={storeDataHandler} />
 			<main className='main'>
-				<Current data={updatedWeatherData} />
+				<Current data={DUMMY_DATA} />
 				<Forecast />
 			</main>
 			<Footer />
